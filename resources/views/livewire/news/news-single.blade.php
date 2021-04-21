70d0673da29f64413683759a8b399345733580e7
MishkaSimakov <msimakov661@gmail.com>
2021-04-21 16:13:47 +0300

    fix news design

--- resources/views/livewire/news/news-single.blade.php
+++ resources/views/livewire/news/news-single.blade.php
@@ -3,38 +3,36 @@
         <h5 class="card-title">{{ $news->title }}</h5>
         <div class="card-text">{!! $news->body !!}</div>
 
-        <div class="row no-gutters text-muted mt-1">
-            <div class="card-text align-self-center">
+        <div class="row no-gutters text-muted mt-1 align-items-center">
+            <div class="mr-auto">
                 {{ $news->date->isoFormat('ll') }}
                 {{ $news->isPublished ? '' : '(не опубликована)'}}
             </div>
 
-            <div class="ml-auto row no-gutters">
-                <span class="align-self-center mr-sm-2" style="font-weight: 500;">
-                    <i class="far fa-eye"></i> {{ $news->views }}
-                </span>
-
-                @canany(['update', 'delete'], $news)
-                    <div class="dropdown">
-                        <button class="d-inline btn rounded-pill text-muted" type="button"
-                                id="news-more-dropdown-button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-h fa-sm"></i>
-                        </button>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="news-more-dropdown-button">
-                            @can('update', $news)
-                                <a class="dropdown-item" href="{{ route('news.edit', $news) }}">Редактировать</a>
-                            @endcan
-                            @can('delete', $news)
-                                <a class="dropdown-item text-danger" href="#" wire:click.prevent="deleteNews">
-                                    Удалить
-                                </a>
-                            @endcan
-                        </div>
-                    </div>
-                @endcanany
+            <div class="mr-sm-2" style="font-weight: 500;">
+                <i class="far fa-eye"></i> {{ $news->views }}
             </div>
+
+            @canany(['update', 'delete'], $news)
+                <div class="dropdown">
+                    <button class="d-inline btn rounded-pill text-muted" type="button"
+                            id="news-more-dropdown-button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-ellipsis-h fa-sm"></i>
+                    </button>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="news-more-dropdown-button">
+                        @can('update', $news)
+                            <a class="dropdown-item" href="{{ route('news.edit', $news) }}">Редактировать</a>
+                        @endcan
+                        @can('delete', $news)
+                            <a class="dropdown-item text-danger" href="#" wire:click.prevent="deleteNews">
+                                Удалить
+                            </a>
+                        @endcan
+                    </div>
+                </div>
+            @endcanany
         </div>
     </div>
 </div>
