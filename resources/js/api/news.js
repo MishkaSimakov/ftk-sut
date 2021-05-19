c54acd960d2f21fe5c13b28d445c5b7ac9d4c47a
MishkaSimakov <msimakov661@gmail.com>
2021-05-19 20:31:20 +0300

    remove unnecessary js files from store and api

--- resources/js/api/news.js
+++
@@ -1,9 +0,0 @@
-export default {
-    async loadNews({ sortType, page }) {
-        return new Promise((resolve, reject) => {
-            axios.get(route('api.news.index', { page: page, sortType: sortType })).then((response) => {
-                resolve(response)
-            })
-        })
-    }
-}
--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import news from './modules/news'
 import rating from './modules/rating'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        news,
         rating
     }
 })
