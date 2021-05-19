2e938f744f2918776d4245246fee9d24897bac3e
MishkaSimakov <msimakov661@gmail.com>
2021-05-19 18:41:11 +0300

    start filling about page

--- config/information.php
+++ config/information.php
@@ -8,8 +8,10 @@ return [
     'ftk' => [
         'email' => 'ftk-sut@yandex.ru',
         'vk' => 'https://vk.com/ftksut',
+        'phone' => '8 (863) 924-28-21',
+        'map_link' => 'https://api-maps.yandex.ru/frame/v1/-/CCQhjZU18B?',
         'discord' => [
-            'invite_link' => ''
+            'invite_link' => 'https://discord.gg/wWD85chdxQ'
         ]
     ]
 ];
